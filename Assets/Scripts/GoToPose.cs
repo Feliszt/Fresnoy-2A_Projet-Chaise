18c951fa73b0e2eb15cf4e1cf8a7e5fd1ab2ad9a
Félix <vmmfelix@gmail.com>
2025-04-02 19:25:32 +0200

    Put the down pulleys higher. Added a target pose and a zero pose.

---
+++ Assets/Scripts/GoToPose.cs
@@ -0,0 +1,74 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GoToPose : MonoBehaviour
+{
+    public Transform targetPose;
+    public Transform zeroPose;
+    private Transform trueTargetPose;
+    private Transform fromPose;
+    [Range(0.00001f, 0.001f)]
+    public float speed = 0.0001f;
+    private float timeCount = 0.0f;
+    public Boolean goToTarget = false;
+    public Boolean goToZero = false;
+    private Boolean go = false;
+    private Boolean goPrev = false;
+    private Boolean going = false;
+    private Boolean goingPrev = false;
+    private float distToTarget;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        trueTargetPose = zeroPose;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        go = goToTarget ^ goToZero;
+
+        if (goToTarget) {
+            trueTargetPose = targetPose;
+            Debug.Log("go to target!");
+        }
+
+        if (goToZero) {
+            trueTargetPose = zeroPose;
+            Debug.Log("go to zero!");
+        }
+
+        if (go && !goPrev) {
+            timeCount = 0.0f;
+            fromPose = transform;
+            goToTarget = false;
+            goToZero = false;
+            go = false;
+            going = true;
+        }
+
+        if (going) {
+
+            transform.position = Vector3.Lerp(fromPose.position, trueTargetPose.position, timeCount * speed);
+            transform.rotation = Quaternion.Lerp(fromPose.rotation, trueTargetPose.rotation, timeCount * speed);
+        }
+
+        distToTarget = Vector3.Distance(transform.position, trueTargetPose.position) + Vector3.Distance(transform.eulerAngles, trueTargetPose.eulerAngles);
+        if (distToTarget <= 0.001f) {
+            going = false;
+            transform.position = trueTargetPose.position;
+            transform.rotation = trueTargetPose.rotation;
+        }
+
+        if (!going && goingPrev) {
+            Debug.Log("arrived at target.");
+        }
+
+        timeCount += Time.deltaTime;
+        goPrev = go;
+        goingPrev = going;
+    }
+}
